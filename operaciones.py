4d854e12c7f81cb24da5a7e20d16b3f02b768dd5
LauraGarciaa1999 <lauragaarciam1999@gmail.com>
2024-01-20 15:39:39 +0100

    Prueba operaciones

---
+++ operaciones.py
@@ -0,0 +1,23 @@
+def sumar(a, b):
+    return a + b
+if __name__ == "__main__":
+    print(sumar(5, 3))
+
+    
+def restar(a, b):
+    return a - b 
+    
+if __name__ == "__main__":
+    print(restar(5, 3))  
+
+
+def multiplicar(a, b):
+    return a * b
+if __name__ == "__main__":
+    print(multiplicar(5, 3))
+
+
+def dividir(a, b):
+    return a / b
+if __name__ == "__main__":
+    print(dividir(5, 3))   
---
+++ test_operaciones.py
@@ -0,0 +1,49 @@
+import unittest
+from operaciones import sumar
+
+class TestSumar(unittest.TestCase):
+    def test_sumar(self):
+        self.assertEqual(sumar(3, 2), 5)
+        self.assertEqual(sumar(-1, 1), 0)
+        self.assertEqual(sumar(-1, -1), -2)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+from operaciones import restar
+
+class TestRestar(unittest.TestCase):
+    def test_restar(self):
+        self.assertEqual(restar(3, 2), 1)
+        self.assertEqual(restar(5, 1), 4)
+        self.assertEqual(restar(6, 1), 5)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+from operaciones import multiplicar
+
+class TestMultiplicar(unittest.TestCase):
+    def test_multiplicar(self):
+        self.assertEqual(multiplicar(3, 2), 6)
+        self.assertEqual(multiplicar(1, 1), 1)
+        self.assertEqual(multiplicar(8, 3), 24)
+
+if __name__ == '__main__':
+    unittest.main()    
+
+
+
+from operaciones import dividir
+
+class TestDividir(unittest.TestCase):
+    def test_dividir(self):
+        self.assertEqual(dividir(3, 2), 1.5)
+        self.assertEqual(dividir(2, 1), 2)
+        self.assertEqual(dividir(20, 10), 2)
+        self.assertEqual(dividir(5, 0), 0)
+
+if __name__ == '__main__':
+    unittest.main()    
